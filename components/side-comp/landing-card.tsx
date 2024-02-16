fd2435f69e94b337ef11f5a362a345df5f933ec5
Israel-sanmi <adetomokunisrael2001@gmail.com>
2024-02-16 04:12:48 +0100

    mobile, tab, pc responsiveness

--- components/side-comp/landing-card.tsx
+++ components/side-comp/landing-card.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import React from "react";
 
-
 interface Componentprops {
   image: any;
   headText: String;
@@ -10,14 +9,18 @@ interface Componentprops {
 
 const LandingCard = ({ image, headText, bodyText }: Componentprops) => {
   return (
-    <div className="md:w-[416px] w-auto h-[200px] md:h-[284px] flex flex-col justify-center p-5 bg-white shadow-md rounded-[16px]">
-      <Image src={image} alt="img" className="md:w-20 w-10 h-10 md:h-20" />
+    <div className="md:max-w-[416px] md:w-full w-auto h-[200px] md:min-h-[284px] flex flex-col justify-center p-5 bg-white shadow-md rounded-[16px]">
+      <Image src={image} alt="img" className="w-10 h-10" />
       <div>
-        <h1 className="md:text-2xl text-lg py-5 font-semibold">{headText}</h1>
-        <p className="md:text-lg text-sm text-[#5E5E5E] font-medium">{bodyText}</p>
+        <h1 className="md:text-2xl sm:text-xl text-lg py-5 font-semibold">{headText}</h1>
+        <p className="md:text-lg sm:text-sm text-xs text-[#5E5E5E] font-medium">
+          {bodyText}
+        </p>
       </div>
     </div>
   );
 };
 
 export default LandingCard;
+
+// md:grid-cols-3 grid-cols-1 sm:grid-cols-2
