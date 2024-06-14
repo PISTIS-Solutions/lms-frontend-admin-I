dc774987e53350858c9f034ec229a8a5902752df
Israel-sanmi <adetomokunisrael2001@gmail.com>
2024-06-14 07:51:12 +0100

    update

--- utils/markdown.tsx
+++ utils/markdown.tsx
@@ -28,7 +28,7 @@ export const code = ({ node, ...props }: any) => (
 export const customLink = ({ node, ...props }: any) => (
   <a
     target="_blank"
-    className="text-blue cursor-pointer bg-main px-2 py-1 m-1"
+    className="text-blue cursor-pointer italic px-2 py-1 m-1"
     {...props}
   />
 );
