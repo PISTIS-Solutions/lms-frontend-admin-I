ff01847157404b7ae1f87698215a240b9c3da002
Israel-sanmi <adetomokunisrael2001@gmail.com>
2024-07-13 08:17:22 +0100

    endpoint repush and check

--- store/fetch-students.ts
+++ store/fetch-students.ts
@@ -156,7 +156,7 @@ const useStudentsStore = create<StudentsStore>((set, get) => ({
       set({ loading: true });
       const adminAccessToken = Cookies.get("adminAccessToken");
       const response = await axios.get(
-        ` https://backend.dev.pististechub.io/api/v1/auth/users/student/?page=${page}`,
+        `https://backend.dev.pististechub.io/api/v1/auth/users/student/?page=${page}`,
         {
           headers: {
             Authorization: `Bearer ${adminAccessToken}`,
