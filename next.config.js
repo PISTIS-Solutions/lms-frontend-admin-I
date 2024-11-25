d713615aa8b3fc3f8bb3117a0e72f01e4ac1e42b
TOLU-MICH <michealtolu2003@gmail.com>
2024-11-25 11:50:02 +0100

    Feature: add image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "cloudinary-2-s3.s3.amazonaws.com", "${{%20secrets.aws_storage_bucket_name%20}}.s3.amazonaws.com"],
+    domains: [
+      "res.cloudinary.com",
+      "cloudinary-2-s3.s3.amazonaws.com",
+      "${{%20secrets.aws_storage_bucket_name%20}}.s3.amazonaws.com",
+      "cloudinary2.s3.amazonaws.com",
+    ],
   },
 };
 
