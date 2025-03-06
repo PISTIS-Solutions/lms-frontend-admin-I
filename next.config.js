46e3a49fc625dfaab3c67ed1a3e94237a1f0f6a2
israel-sanmi <ilesanmiisrael@outlook.com>
2025-03-06 08:22:18 +0100

    docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: [
       "res.cloudinary.com",
       "cloudinary-2-s3.s3.amazonaws.com",
